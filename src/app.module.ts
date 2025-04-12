927e212ae96577422e5f07f297c2847371ec9451
Grzegorz Dubiel <greg2012201@gmail.com>
2025-04-12 23:24:24 +0200

    Import encryption service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { EncryptionModule } from './encryption/encryption.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
     DrizzleModule,
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    EncryptionModule,
   ],
   controllers: [],
   providers: [],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,13 @@ import { UserService } from './user.service';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
 import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
+import { EncryptionModule } from 'src/encryption/encryption.module';
+import { EncryptionService } from 'src/encryption/encryption.service';
 
 @Module({
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
-  imports: [DrizzleModule],
+  imports: [DrizzleModule, EncryptionModule],
   controllers: [UserController],
 })
 export class UserModule {}
