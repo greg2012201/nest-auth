e7bf3874fe1862f49b3bb69cc4009efd008215c4
Grzegorz Dubiel <greg2012201@gmail.com>
2025-04-14 21:42:37 +0200

    Fix home route

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { EncryptionModule } from './encryption/encryption.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -13,7 +15,7 @@ import { EncryptionModule } from './encryption/encryption.module';
     UserModule,
     EncryptionModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
