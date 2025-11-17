40517c5387540d83418efb5166cee18acf468c32
greg2012201 <greg2012201@gmail.com>
2025-11-17 06:36:09 +0100

    Cleanup.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
 import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
 import { EncryptionModule } from 'src/encryption/encryption.module';
-import { EncryptionService } from 'src/encryption/encryption.service';
 
 @Module({
-  providers: [UserService, UserRepository],
-  exports: [UserService, UserRepository],
+  providers: [UserRepository],
+  exports: [UserRepository],
   imports: [DrizzleModule, EncryptionModule],
   controllers: [UserController],
 })
